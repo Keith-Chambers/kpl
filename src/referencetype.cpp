7f64fb9b22a388c5c00408a7a75bf82130713c1b
kd.chambers97@gmail.com <kd.chambers97@gmail.com>
2019-02-25 22:27:15 +0000

    Added ReferenceType

--- src/referencetype.cpp
+++ src/referencetype.cpp
@@ -1,6 +1,19 @@
-#include "referencetype.h"
+#include "kpl/referencetype.h"
 
-ReferenceType::ReferenceType()
-{
+namespace kpl {
+    namespace reflection {
 
+        ReferenceType::ReferenceType(kpl::reflection::DataType dataType)
+            : kpl::reflection::DataType( dataType.name() + "&", dataType.sizeBits(), dataType.isPrimative(), false, true ), mDataType { dataType }
+        {
+
+        }
+
+        DataType ReferenceType::getSubType() const
+        {
+            return mDataType;
+        }
+    }
 }
+
+

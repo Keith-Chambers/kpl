ef50bfb6d94288522ddac6d4238b840406d8d2a9
kd.chambers97@gmail.com <kd.chambers97@gmail.com>
2019-02-27 00:34:19 +0000

    ?

--- src/memberfunction.cpp
+++ src/memberfunction.cpp
@@ -1,6 +1,51 @@
-#include "memberfunction.h"
+#include "kpl/memberfunction.h"
 
-MemberFunction::MemberFunction()
-{
+#include <kpl/class.h>
 
+namespace kpl {
+    namespace reflection {
+
+        MemberFunction::MemberFunction( const kpl::reflection::Class& classRef, const kpl::reflection::Function function,
+                        kpl::reflection::AccessModifier accessModifier, bool isStatic, bool isConst )
+            : mClass { classRef }, mFunction { function }, mAccessModifier { accessModifier }, mIsStatic { isStatic }, mIsConst { isConst }
+        {
+            assert( ! ( mIsConst && mIsStatic ) && "Method cannot be both const and static" );
+        }
+
+        std::string MemberFunction::asDeclarationCode(bool fullyQualify) const
+        {
+            static const std::string staticQualifier = ( mIsStatic ) ? "static" : "";
+            static const std::string constQualifier = ( mIsConst ) ? "const" : "";
+            const std::string classQualifier = (fullyQualify) ? mClass.classNamespace() + "::" : "";
+
+            return fmt::format("{} {} {}{}({}) {};", staticQualifier, mFunction.returnValueString(), classQualifier, mFunction.name(), mFunction.parameterListString(), constQualifier);
+        }
+
+        std::string MemberFunction::asInvocationCode(const std::string instanceName, const std::string passedParameterString, bool isPointer) const
+        {
+            const std::string accessNotation = ( isPointer ) ? "->" : ".";
+
+            return fmt::format("{}{}{}({})", instanceName, accessNotation, mFunction.name(), passedParameterString);
+        }
+
+        const kpl::reflection::Function MemberFunction::funcData() const
+        {
+            return mFunction;
+        }
+
+        const kpl::reflection::Class& MemberFunction::classData() const
+        {
+            return mClass;
+        }
+
+        bool MemberFunction::isStatic() const
+        {
+            return mIsStatic;
+        }
+
+        bool MemberFunction::isConst() const
+        {
+            return mIsConst;
+        }
+    }
 }
--- src/membervariable.cpp
+++ src/membervariable.cpp
@@ -1,6 +1,83 @@
-#include "membervariable.h"
+#include "kpl/membervariable.h"
 
-MemberVariable::MemberVariable()
-{
+#include <kpl/class.h>
 
+namespace kpl {
+    namespace reflection {
+
+        MemberVariable::MemberVariable(const kpl::reflection::Class& classRef,
+                                       const kpl::reflection::Variable& variable,
+                                       kpl::reflection::AccessModifier accessModifier,
+                                       bool isStatic)
+            : mClass { classRef }, mVariable { variable }, mAccessModifier { accessModifier }, mIsStatic { isStatic }
+        {
+            assert( ! variable.name().empty() );
+        }
+
+        std::string MemberVariable::defaultInstanceName() const
+        {
+            std::string result = mVariable.name();
+
+            result = Util::upperFirstChar(result);
+            result.insert(result.begin(), 'm');
+
+            return result;
+        }
+
+        std::string MemberVariable::asGetterMethodDeclaration(bool fullyQualify) const
+        {
+            const std::string qualifier = ( fullyQualify ) ? mClass.classNamespace() + "::" : "";
+
+            return fmt::format("const {}& {}{}() const;", mVariable.typeName(), qualifier, mVariable.name());
+        }
+
+        std::string MemberVariable::asGetterMethodDefinition(std::string instanceName, bool fullyQualify) const
+        {
+            bool defaultNameUsed = false;
+
+            if(instanceName.empty())
+            {
+                instanceName = defaultInstanceName();
+                defaultNameUsed = true;
+            }
+
+            std::string getterMethodName = instanceName;
+
+            // If using the mTypeName convention, remove the m and lowercase first char
+            if(defaultNameUsed)
+            {
+                getterMethodName.erase(0, 1);
+                getterMethodName = kpl::Util::lowerFirstChar(getterMethodName);
+            }
+
+            const std::string qualifier = ( fullyQualify ) ? mClass.classNamespace() + "::" : "";
+
+            return fmt::format("{} {}{}(){ return {}; }", mVariable.typeName(), qualifier, getterMethodName, mVariable.name());
+        }
+
+        std::string MemberVariable::fullyQualifiedName() const
+        {
+            return mClass.classNamespace() + "::" + mVariable.name();
+        }
+
+        const kpl::reflection::Variable MemberVariable::varData() const
+        {
+            return mVariable;
+        }
+
+        const kpl::reflection::Class MemberVariable::classData() const
+        {
+            return mClass;
+        }
+
+        const kpl::reflection::AccessModifier& MemberVariable::accessModifier() const
+        {
+            return mAccessModifier;
+        }
+
+        const std::string& MemberVariable::accessModifierString() const
+        {
+            return kpl::reflection::Class::AccessModifierString( mAccessModifier );
+        }
+    }
 }

dd8933850af2475c1ae6baa0add055a607997f83
kd.chambers97@gmail.com <kd.chambers97@gmail.com>
2019-03-06 17:55:00 +0000

    Added createFile and truncateFile

--- include/kpl/kfile.h
+++ include/kpl/kfile.h
@@ -4,10 +4,14 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <filesystem>
+#include <iostream>
 
 namespace kpl
 {
     bool fileExists(const std::string& filePath);
     std::string loadTextFile(std::string filePath);
+    bool createFile(const std::filesystem::path, bool force);
+    bool truncateFile(const std::filesystem::path path);
 }
 #endif // KFILE_H
--- src/kfile.cpp
+++ src/kfile.cpp
@@ -22,4 +22,34 @@ namespace kpl
         // Make sure to copy construct, may not be needed
         return std::string(buffer.str());
     }
+
+
+    bool createFile(const std::filesystem::path path, bool force)
+    {
+        if(fileExists(path) && ! force)
+        {
+            std::cout << path << " already exists. Exiting" << std::endl;
+            return false;
+        }
+
+        std::ofstream newFile (path);
+        newFile.close();
+
+        return true;
+    }
+
+    bool truncateFile(const std::filesystem::path path)
+    {
+        if(! fileExists(path))
+        {
+            std::cout << path << " doesn't exist. Exiting truncateFile function" << std::endl;
+            return false;
+        }
+
+        std::ofstream truncateStream;
+        truncateStream.open(path, std::ofstream::trunc);
+        truncateStream.close();
+
+        return true;
+    }
 }
